825e860c75713c3f181db8e78c3a925eecc70a37
Milton Munoz <livemilton@gmail.com>
2025-12-12 06:41:05 -0500

    React wiht ts :sparkles: Adding a calendar defines the spending date

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,5 +1,11 @@
 
 import { categories } from "../data/categories";
+import DatePicker from 'react-date-picker';
+import 'react-calendar/dist/Calendar.css';
+import 'react-date-picker/dist/DatePicker.css';
+
+type ValuePiece = Data | null;
+ type Value = ValuePiece | [ValuePiece, ValuePiece]
 
 export default function ExpenseForm() {
   return (
@@ -55,6 +61,15 @@ export default function ExpenseForm() {
                 >{category.name}</option>
             ))}
         </select>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Fecha Gasto:
+        </label>
+        <DatePicker 
+            className="bg-slate-100 p-2 border-0"
+        />
       </div>
 
       <input 
