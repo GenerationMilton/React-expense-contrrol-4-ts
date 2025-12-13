2838727d0ba0d58e4f412769ce5544e145904ae2
Milton Munoz <livemilton@gmail.com>
2025-12-13 10:01:10 -0500

    React wiht ts :sparkles: Showing an alert if form have a wrong validation

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -5,6 +5,7 @@ import 'react-calendar/dist/Calendar.css';
 import 'react-date-picker/dist/DatePicker.css';
 import { useState, type ChangeEvent } from "react";
 import type { DraftExpense, Value } from "../types";
+import ErrorMessage from "./ErrorMessage";
 
 
 export default function ExpenseForm() {
@@ -15,6 +16,8 @@ export default function ExpenseForm() {
         category: '',
         date: new Date()
     })
+
+    const [error, setError] = useState('')
 
     const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
         const { name, value} = e.target
@@ -32,14 +35,27 @@ export default function ExpenseForm() {
         })
     }
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        // Validation
+        if(Object.values(expense).includes('')){
+            setError('Todos los campos son obligatorios')
+            return
+        }
+        console.log('todo bien...')
+    }
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <legend
         className="uppercase text-center text-2xl font-black border-b-4
             border-blue-500 py-2"
       >
         Nuevo Gasto
       </legend>
+
+      {error && <ErrorMessage>{error}</ErrorMessage>}
 
       <div className="flex flex-col gap-2">
         <label htmlFor="expenseName" className="text-xl">
