61d4c5012027eb059c95fbe585e4975da0482ee6
Milton Munoz <livemilton@gmail.com>
2025-12-17 22:09:14 -0500

    React with ts :sparkles: Editing the expense

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -52,8 +52,13 @@ export default function ExpenseForm() {
             setError('Todos los campos son obligatorios')
             return
         }
-        // Agregar un nuevo gasto
-        dispatch({type: 'add-expense', payload: {expense}})
+        // Agregar o actualizar el gasto
+        if(state.editingId){
+          dispatch({type: 'update-expense', payload: {expense: {id: state.editingId, ...expense }}})
+        }else {
+          dispatch({type: 'add-expense', payload: {expense}})
+        }
+        
 
         // Reiniciar el state
         setExpense({
